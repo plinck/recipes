6931b883073a4fb75979e4f1ce492a55b7b545b0
Paul Linck <paul.linck@gmail.com>
2018-12-19 19:23:02 -0500

    updated routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,20 @@
 import Vapor
 
+// class for JSON encoding - content provides this
+final class Dish : Content {
+    var title: String
+    var price: Double
+    var description: String
+    var course: String
+    
+    init (title: String, price: Double, course: String, description: String ) {
+        self.title = title
+        self.price = price
+        self.description = description
+        self.course = course
+    }
+} // Dish
+
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "It works" example
@@ -7,14 +22,17 @@ public func routes(_ router: Router) throws {
         return "It works!"
     }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+    router.get("dish") { req -> [Dish] in
+        var dishes: [Dish] = []
+        
+        dishes.append(Dish(title: "Spaghetti", price: 5.50, course: "Entree", description: "Spaghetti and meatballs"))
+        dishes.append(Dish(title: "Braised Lamb", price: 9.95, course: "Entree", description: "Braised Lamb Shanks"))
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
-}
+        return dishes
+    }
+    
+    router.post(Dish.self, at: "api/dish") {req, data -> String in
+        
+        return "Put in a dish"
+    }
+ }
